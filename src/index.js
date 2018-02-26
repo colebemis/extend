d81277159dcfcde6080505f0add11ba1bd1862a7
Cole Bemis <colebemis@gmail.com>
2018-02-26 10:14:12 -0800

    Display installed extensions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { render } from 'react-dom'
 
-const App = () => (
-  <div>Hello React</div>
-)
+class App extends Component {
+  state = {
+    loading: true,
+    extensions: [],
+  }
 
-render(<App />, document.getElementById('root'))
+  componentDidMount() {
+    this.props.chrome.management.getAll(extensions =>
+      this.setState({
+        loading: false,
+        extensions,
+      }),
+    )
+  }
+
+  render() {
+    return (
+      <div style={{ width: 360 }}>
+        {this.state.extensions.map(extension => (
+          <div key={extension.id}>
+            <input
+              id={extension.id}
+              type="checkbox"
+              checked={extension.enabled}
+            />
+            <label htmlFor={extension.id}>{extension.shortName}</label>
+          </div>
+        ))}
+      </div>
+    )
+  }
+}
+
+render(<App chrome={chrome} />, document.getElementById('root'))
