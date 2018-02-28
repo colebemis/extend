72c54f8eff4393992611b865dd08d327ba2af077
Cole Bemis <colebemis@gmail.com>
2018-02-27 17:41:14 -0800

    Use named exports

--- src/theme.js
+++ src/theme.js
@@ -1,15 +1,9 @@
-const colors = {
+export const colors = {
   blue: '#08f',
   black: '#000',
   white: '#fff',
 }
 
-const opacities = [0, 0.1, 0.2, 0.4, 0.6, 0.8, 1]
+export const opacities = [0, 0.1, 0.2, 0.4, 0.6, 0.8, 1]
 
-const radius = 2
-
-export default {
-  colors,
-  opacities,
-  radius,
-}
+export const radius = 2
