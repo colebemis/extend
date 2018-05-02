74a9f9784d619b5ca986fd3c9d8a58dfcef1064d
Cole Bemis <colebemis@gmail.com>
2018-05-01 23:29:11 -0700

    Add joinSpacing function

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,11 @@ function isNumber(value) {
 export function toPx(value) {
   return isNumber(value) && value !== 0 ? value + 'px' : value
 }
+
+export function joinSpacing(...args) {
+  if (args.length > 4) {
+    throw new Error('Too many arguments')
+  }
+
+  return args.map(toPx).join(' ')
+}
