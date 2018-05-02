e0f397c8711f290de9d36d367ec91a2ca4d0cc81
Cole Bemis <colebemis@gmail.com>
2018-05-01 22:49:50 -0700

    Switch to glamorous

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,76 +1,79 @@
 import React from 'react'
-import { bool, string, oneOfType, number } from 'prop-types'
-import styled, { css } from 'styled-components'
+import { bool, string, oneOfType, number, func } from 'prop-types'
+import glamorous, { Div } from 'glamorous'
 import { hideVisually, rgba } from 'polished'
-import { toPx } from '../utils'
 import { colors, opacities, radius } from '../theme'
 
-const Container = styled.div`
-  display: inline-block;
-`
-
-const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
-  ${hideVisually()};
-`
-
-const Icon = styled.svg`
-  fill: none;
-  stroke: ${colors.white};
-  stroke-width: 1.5px;
-`
-
-const uncheckedStyles = css`
-  background-color: ${props =>
-    props.disabled ? rgba(colors.black, opacities[1]) : colors.white};
-  border-color: ${props =>
-    props.disabled
-      ? rgba(colors.black, opacities[1])
-      : rgba(colors.black, opacities[2])};
-
-  ${Icon} {
-    visibility: hidden;
-  }
-`
-
-const checkedStyles = css`
-  background-color: ${props =>
-    props.disabled ? rgba(colors.black, opacities[3]) : colors.blue};
-  border-color: transparent;
-`
-
-const StyledCheckbox = styled.div`
-  box-sizing: border-box;
-  width: ${props => toPx(props.size)};
-  height: ${props => toPx(props.size)};
-  border-width: 1px;
-  border-style: solid;
-  border-radius: ${toPx(radius)};
-  transition: all 150ms;
-
-  ${props => (props.checked ? checkedStyles : uncheckedStyles)};
-
-  ${HiddenCheckbox}:focus + & {
-    border-color: ${colors.blue};
-    box-shadow: 0 0 0 3px ${rgba(colors.blue, opacities[3])};
-  }}
-`
-
-const Checkbox = ({ className, checked, disabled, size, ...props }) => (
-  <Container className={className}>
-    <HiddenCheckbox checked={checked} disabled={disabled} {...props} />
-    <StyledCheckbox checked={checked} disabled={disabled} size={size}>
-      <Icon viewBox="0 0 24 24">
-        <polyline vectorEffect="non-scaling-stroke" points="20 6 9 17 4 12" />
-      </Icon>
-    </StyledCheckbox>
-  </Container>
+const hiddenCheckboxClassName = 'hidden-checkbox'
+const HiddenCheckbox = glamorous('input', { withProps: { type: 'checkbox' } })(
+  hiddenCheckboxClassName,
+  hideVisually,
 )
+
+const iconClassName = 'icon'
+const Icon = glamorous.svg(iconClassName, {
+  fill: 'none',
+  stroke: colors.white,
+  strokeWidth: 1.5,
+})
+
+const uncheckedStyle = props => ({
+  backgroundColor: props.disabled
+    ? rgba(colors.black, opacities[1])
+    : colors.white,
+  borderColor: props.disabled
+    ? rgba(colors.black, opacities[1])
+    : rgba(colors.black, opacities[2]),
+
+  [`& .${iconClassName}`]: {
+    visibility: 'hidden',
+  },
+})
+
+const checkedStyle = props => ({
+  backgroundColor: props.disabled
+    ? rgba(colors.black, opacities[3])
+    : colors.blue,
+  borderColor: 'transparent',
+})
+
+const StyledCheckbox = glamorous.div(
+  props => ({
+    boxSizing: 'border-box',
+    width: props.size,
+    height: props.size,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderRadius: radius,
+    transition: 'all 100ms',
+
+    [`.${hiddenCheckboxClassName}:focus + &`]: {
+      borderColor: colors.blue,
+      boxShadow: `0 0 0 3px ${rgba(colors.blue, opacities[3])}`,
+    },
+  }),
+  props => (props.checked ? checkedStyle : uncheckedStyle),
+)
+
+function Checkbox({ className, checked, disabled, size, ...props }) {
+  return (
+    <Div className={className} display="inline-block">
+      <HiddenCheckbox checked={checked} disabled={disabled} {...props} />
+      <StyledCheckbox checked={checked} disabled={disabled} size={size}>
+        <Icon viewBox="0 0 24 24">
+          <polyline vectorEffect="non-scaling-stroke" points="20 6 9 17 4 12" />
+        </Icon>
+      </StyledCheckbox>
+    </Div>
+  )
+}
 
 Checkbox.propTypes = {
   className: string,
   checked: bool,
   disabled: bool,
   size: oneOfType([number, string]),
+  onChange: func,
 }
 
 Checkbox.defaultProps = {
@@ -78,6 +81,7 @@ Checkbox.defaultProps = {
   checked: false,
   disabled: false,
   size: 16,
+  onChange: () => {},
 }
 
 export default Checkbox
