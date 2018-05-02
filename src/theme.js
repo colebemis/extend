4bc2ea3a6f758aab7d6518d94bce3b5ff7245ff5
Cole Bemis <colebemis@gmail.com>
2018-05-01 22:50:02 -0700

    Update theme

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,9 @@
 export const colors = {
-  blue: '#08f',
+  blue: '#05f',
   black: '#000',
   white: '#fff',
 }
 
 export const opacities = [0, 0.1, 0.2, 0.4, 0.6, 0.8, 1]
 
-export const radius = 2
+export const radius = 3
